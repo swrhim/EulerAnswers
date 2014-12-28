9236b52cc5f07d8405df894e56df48641942da94
swrhim <swrhim@gmail.com>
2014-12-28 16:31:25 -0500

    Solution to problem 7

--- Problem7.py
+++ Problem7.py
@@ -1,1 +1,25 @@
 __author__ = 'Daniel'
+import math
+
+def FindNthPrime(position):
+    # skip 2 since it's the only prime that's even
+    counter = 2
+    possiblePrime = 3
+    while counter < position:
+        possiblePrime += 2
+
+        isPrimeFlag = IsPrime(possiblePrime)
+
+        if isPrimeFlag:
+            counter += 1
+
+    return possiblePrime
+
+def IsPrime(num):
+    maxNum = int(math.ceil(math.sqrt(num)))
+    for i in range(2, maxNum+1):
+        if num % i == 0:
+            return False
+    return True
+
+print FindNthPrime(10001)
